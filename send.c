d3d0a1f1df346c6cb0e5627fed094eb7b3a028cb
Tobias Rehbein <tobias.rehbein@web.de>
2013-03-05 19:15:13 +0100

    Get rid of an unnecessary carriage return

--- send.c
+++ send.c
@@ -60,7 +60,7 @@ send_fake_item(FILE *out, char type, const char *info, const char *detail)
 	if (display == NULL) {
 		/* Explicitely do not use gopherized messages to avoid
 		 * recursions. */
-		fprintf(stderr, "malloc: %s\r\n", strerror(errno));
+		fprintf(stderr, "malloc: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 
